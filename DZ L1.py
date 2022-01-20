7f4feb6efd93338187836231d2e978be00645ecc
KseniyaB1404 <brykova.kseniy@gmail.com>
2022-01-20 20:05:40 +0200

    home work 1

---
+++ DZ L1.py
@@ -0,0 +1,40 @@
+# 2 Задача: Создать две переменные first=10, second=30.
+# Вывести на экран построчно результат математического
+# взаимодействия (+, -, *, /, **) для этих чисел.
+first = 10
+second = 30
+third = first + second
+fouth = first - second
+fifth = first * second
+sixth = first / second
+seventh = first ** second
+eighth = second - first
+ninth = second / first
+tenth = second ** first
+print (third)
+print (fouth)
+print (fifth)
+print (round (sixth,2))
+print (seventh)
+print (eighth)
+print (ninth)
+print (tenth)
+
+# Разобраться как работают операторы % и //. Вывести на экран эти операции для переменных из пункта 2.
+eleventh = first % second
+twelfth = first // second
+thirteenth = second % first
+fourteenth = second // first
+print (eleventh)
+print (twelfth)
+print (thirteenth)
+print (fourteenth)
+
+# 1. Чем занимаетесь: кем работаете или на кого учитесь?
+#    Я являюсь co-founder и СОО консалтингового агенства VR|Change agency,
+#    по образованию медсестра и за плечами 11 лет стажа операционной медсестры,
+#    2,5 из которых была заместителем медицинского директора по медсестринству в косметологической клинике
+
+# 2. Для каких целей хотите освоить Python?
+#    Всегда хотела работать в IT-индустрии, но не было ни времени, ни ресурса, чтобы учиться.
+#    Python это одна из ступеней на пути к исполнению давнего желания и приобритению новой профессии
